b8dab7255291214069777a2595924ae7ccdcc98b
Dmytro Nikolayev <dnikolayev@gmail.com>
2018-07-05 20:04:46 +0300

    Update build and test scripts

--- build.ps1
+++ build.ps1
@@ -3,5 +3,5 @@ Write-Host Starting build
 
 cd latest
 docker build --pull -t latest -f Dockerfile .
-
+docker tag latest dnikolayev/sonarqube-mysql-windows:latest
 docker images
---
+++ test.ps1
@@ -0,0 +1,24 @@
+Write-Host Starting test
+
+if ($env:ARCH -ne "amd64") {
+  Write-Host "Arch $env:ARCH detected. Skip testing."
+  exit 0
+}
+
+$ErrorActionPreference = 'SilentlyContinue';
+docker kill dnikolayev/sonarqube-mysql-windows:latest
+docker rm -f dnikolayev/sonarqube-mysql-windows:latest
+
+$ErrorActionPreference = 'Stop';
+Write-Host Starting container
+docker run --name sonar  -it -p 9000:9000  `
+-v C:/data/mysql:C:/MySQL/data `
+-v C:/data/extensions:C:/sonarqube/extensions `
+dnikolayev/sonarqube-mysql-windows:latest
+Start-Sleep 10
+
+docker logs dnikolayev/sonarqube-mysql-windows:latest
+
+$ErrorActionPreference = 'SilentlyContinue';
+docker kill dnikolayev/sonarqube-mysql-windows:latest
+docker rm -f dnikolayev/sonarqube-mysql-windows:latest
