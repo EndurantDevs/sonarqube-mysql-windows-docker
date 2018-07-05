ca5db9f6b70a9127461e0b8a63a60c4a8faac58d
Dmytro Nikolayev <dnikolayev@gmail.com>
2018-07-05 22:14:32 +0300

    Update tests for Appveyor

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
-docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
+docker login -u $env:DOCKER_USER --password-stdin $env:DOCKER_PASS
 docker push dnikolayev/sonarqube-mysql-windows:latest
 docker logout
