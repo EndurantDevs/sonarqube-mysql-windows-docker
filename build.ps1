2149fe070556e241da776835dce86fef6889b27d
Dmytro Nikolayev <dnikolayev@gmail.com>
2018-07-06 00:20:53 +0300

    Debud builder

--- build.ps1
+++ build.ps1
@@ -4,6 +4,8 @@ Write-Host Starting build
 $tags = $env:TAGS -split ';'
 $tags |foreach {
     Write-Host Building... $_
+    Write-Host Free Disk space:
+    fsutil volume diskfree c:
     cd $_
     docker build --pull -t dnikolayev/sonarqube-mysql-windows:$_ -f Dockerfile .
     docker images
