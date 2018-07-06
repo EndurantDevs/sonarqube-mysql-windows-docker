540c00c3ec15391a8c4d3cf808a1ab5efbb01c73
Dmytro Nikolayev <dnikolayev@gmail.com>
2018-07-06 11:21:30 +0300

    Convert build to Matrix build

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ Write-Host Starting build
 Write-Host Building... $env:TAG
 Write-Host Free Disk space:
 fsutil volume diskfree c:
-cd $_
+cd $env:TAG
 docker build --pull -t dnikolayev/sonarqube-mysql-windows:$env:TAG -f Dockerfile .
 docker images
 cd ..
